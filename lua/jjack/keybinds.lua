45d7be0285722a8a75032ef1ec82c25fa914c228
Jack Jefferson <immxral.j4ck@gmail.com>
2025-01-05 20:05:48 +0000

    Made some small updates to the config.

--- lua/jjack/keybinds.lua
+++ lua/jjack/keybinds.lua
@@ -1,5 +1,10 @@
 -- KEYBINDS
 
+-- Neotree
+
+vim.api.nvim_set_keymap('n', '<leader>t', '<Cmd>Neotree toggle<CR>', { noremap = true, silent = true })
+
+
 -- Disabling the arrow keys.
 
 vim.api.nvim_set_keymap('n', '<Up>', '<Nop>', { noremap = true, silent = true })
@@ -16,4 +21,3 @@ vim.api.nvim_set_keymap('v', '<Up>', '<Nop>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('v', '<Down>', '<Nop>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('v', '<Left>', '<Nop>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('v', '<Right>', '<Nop>', { noremap = true, silent = true })
-
