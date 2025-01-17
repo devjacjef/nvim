b3fa08ce6fe43f34bf9e02f9d32accf3bd6bb37b
Jack Jefferson <immxral.j4ck@gmail.com>
2025-01-17 10:38:18 +0000

    Had to move mason and lspconfig back together.

--- lua/jjack/plugins/editor/note-taking.lua
+++ lua/jjack/plugins/editor/note-taking.lua
@@ -1,6 +1,6 @@
 return {
-    'MeanderingProgrammer/render-markdown.nvim',
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' },
-    "folke/zen-mode.nvim",
-    opts = {},
+	'MeanderingProgrammer/render-markdown.nvim',
+	dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' },
+	"folke/zen-mode.nvim",
+	opts = {},
 }
