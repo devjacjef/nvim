d1284a25adcd28c4af779a8bbdac3934d8afce72
Jack Jefferson <dev.jac.jef@proton.me>
2026-05-14 16:53:04 +0100

    feat: adds script to install/compile neovim

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-echo "Instaling Neovim from source."
+# Cloning Neovim to /tmp/
+git clone https://github.com/neovim/neovim /tmp/nvim
+
+# Building Neovim
+cd /tmp/nvim  
+git checkout stable
+make CMAKE_BUILD_TYPE=RelWithDebInfo
+
+# Installing Neovim for Debian
+cd build && cpack -G DEB && sudo dpkg -i nvim-linux-x86_64.deb
